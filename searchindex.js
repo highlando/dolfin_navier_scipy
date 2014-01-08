4c2fcf7307df600e622ba24440c9b2763023da73
highlando <jnhlnd@gmail.com>
2014-01-08 21:30:08 +0100

    now it works hopefully

--- searchindex.js
+++ searchindex.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 Search.setIndex({objects:{},terms:{index:0,search:0,scipi:0,welcom:0,modul:0,navier:0,indic:0,content:0,code:[0,1],tabl:0,document:0,page:0,dolfin:0,refer:[0,1]},objtypes:{},titles:["Welcome to dolfin navier scipy&#8217;s documentation!","Code Reference"],objnames:{},filenames:["index","Code"]})
-=======
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,dolfin_navier_scipi:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to dolfin_navier_scipy&#8217;s documentation!"],objnames:{},filenames:["index"]})
->>>>>>> 662ea23b1fbe00e9dc7eba48b90f71b962c1d380
