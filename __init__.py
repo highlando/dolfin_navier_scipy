5c5a5050700736fd794b6c002ca5afca2eccdc01
highlando <jnhlnd@gmail.com>
2014-01-28 09:36:46 +0100

    __init__ upd

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from . import dolfin_to_sparrays
 from . import data_output_utils
 from . import problem_setups
+from . import stokes_navier_utils
+from . import cont_obs_utils
 
 __all__ = ["dolfin_to_sparrays",
            "data_output_utils",
            "stokes_navier_utils",
-           "problem_setups"]
+           "problem_setups",
+           "cont_obs_utils"]
