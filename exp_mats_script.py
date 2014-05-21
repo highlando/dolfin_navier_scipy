41a5972c12a983f50867068dc7b35fb80b916fd9
highlando <jnhlnd@gmail.com>
2014-05-21 11:18:02 +0200

    try drivcav

--- exp_mats_script.py
+++ exp_mats_script.py
@@ -3,7 +3,8 @@ from get_exp_nsmats import comp_exp_nsmats
 # mddir = '/afs/mpi-magdeburg.mpg.de/data/csc/projects/qbdae-nse/data/'
 mddir = 'data/'
 
-for N in [0]:  # , 1, 2]:
+for N in [10]:  # , 1, 2]:
     for Re in [1e2, 2e2, 3e2, 4e2, 5e2]:
-        comp_exp_nsmats(problemname='cylinderwake', N=N, Re=Re,
+        # comp_exp_nsmats(problemname='cylinderwake', N=N, Re=Re,
+        comp_exp_nsmats(problemname='drivencavity', N=N, Re=Re,
                         mddir=mddir, linear_system=False)
