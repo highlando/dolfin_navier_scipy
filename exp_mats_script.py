3b61f8d9a4fcb14ad4875a935cc75af78bbbb323
highlando <jnhlnd@gmail.com>
2014-05-27 16:41:25 +0200

    revd diss

--- exp_mats_script.py
+++ exp_mats_script.py
@@ -3,8 +3,9 @@ from get_exp_nsmats import comp_exp_nsmats
 # mddir = '/afs/mpi-magdeburg.mpg.de/data/csc/projects/qbdae-nse/data/'
 mddir = 'data/'
 
-for N in [10]:  # , 1, 2]:
-    for Re in [1e2, 2e2, 3e2, 4e2, 5e2]:
+relist = [x*10**y for x in range(1,14,2) for y in [2]]
+for N in [5]: #10, 15, 20, 25]:  # , 1, 2]:
+    for Re in relist:
         # comp_exp_nsmats(problemname='cylinderwake', N=N, Re=Re,
         comp_exp_nsmats(problemname='drivencavity', N=N, Re=Re,
                         mddir=mddir, linear_system=False)
