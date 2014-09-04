a1d28da38acf9bffa273fbd661e8fafaaf389fd0
highlando <jnhlnd@gmail.com>
2014-09-04 16:52:39 +0200

    now p output mat exported

--- exp_mats_script.py
+++ exp_mats_script.py
@@ -3,8 +3,9 @@ from get_exp_nsmats import comp_exp_nsmats
 # mddir = '/afs/mpi-magdeburg.mpg.de/data/csc/projects/qbdae-nse/data/'
 mddir = 'data/'
 
-relist = [x*10**y for x in range(1, 14, 2) for y in [2]]
-for N in [1, 2, 3]:  # 10, 15, 20, 25]:  # , 1, 2]:
+# relist = [x*10**y for x in range(1, 14, 2) for y in [2]]
+relist = [x*10**y for x in range(1, 6) for y in [2]]
+for N in [0, 1, 2]:  # 10, 15, 20, 25]:  # , 1, 2]:
     for Re in relist:
         comp_exp_nsmats(problemname='cylinderwake', N=N, Re=Re,
         # comp_exp_nsmats(problemname='drivencavity', N=N, Re=Re,
