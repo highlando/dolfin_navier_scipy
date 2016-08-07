b7e2a9b754e997e5d88091595ccbabb40eecbe5a
highlando <jnhlnd@gmail.com>
2016-08-07 15:52:11 +0200

    upd to make `read the docs` work again

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../dolfin_navier_scipy'))
 
 # According to hint from
 # <http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/>.
