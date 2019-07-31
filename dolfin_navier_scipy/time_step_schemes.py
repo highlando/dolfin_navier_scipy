e5fa2a3ffa15ee68b6fd2802fd0b1309402c2c2c
highlando <jnhlnd@gmail.com>
2019-07-31 07:45:26 +0200

    a dynamic rhs (like an observer) for the time stepping scheme

--- dolfin_navier_scipy/time_step_schemes.py
+++ dolfin_navier_scipy/time_step_schemes.py
@@ -10,7 +10,7 @@ def cnab(trange=None, inivel=None, inip=None, bcs_ini=[],
          M=None, A=None, J=None, nonlvfunc=None,
          fv=None, fp=None, scalep=-1.,
          getbcs=None, applybcs=None, appndbcs=None,
-         savevp=None,
+         savevp=None, dynamic_rhs=None, dynamic_rhs_memory={},
          ntimeslices=10, verbose=True):
 
     dtvec = np.array(trange)[1:] - np.array(trange)[:-1]
@@ -24,6 +24,12 @@ def cnab(trange=None, inivel=None, inip=None, bcs_ini=[],
 
     dt = trange[1] - trange[0]
     NP, NV = J.shape
+    zerorhs = np.zeros((NV, 1))
+
+    if dynamic_rhs is None:
+        def dynamic_rhs(t, cv=None, memory={}):
+            return zerorhs, memory
+    drm = dynamic_rhs_memory
 
     savevp(appndbcs(inivel, bcs_ini), inip, time=trange[0])
 
@@ -31,31 +37,33 @@ def cnab(trange=None, inivel=None, inip=None, bcs_ini=[],
     bfv_c, bfp_c, mbc_c = applybcs(bcs_c)
     fv_c = fv(trange[0])
     nfc_c = nonlvfunc(appndbcs(inivel, bcs_ini))
+    dfv_c, drm = dynamic_rhs(trange[0], vc=inivel, memory=drm, mode='init')
+    tdfv, drm = dynamic_rhs(trange[0], vc=inivel, memory=drm, mode='heunpred')
 
-    bcs_n = getbcs(trange[1], appndbcs(inivel, bcs_ini), inip, mode='heunpred')
-    bfv_n, bfp_n, mbc_n = applybcs(bcs_n)
+    tbcs = getbcs(trange[1], appndbcs(inivel, bcs_ini), inip, mode='heunpred')
+    tbfv, tbfp, tmbc = applybcs(tbcs)
     fv_n, fp_n = fv(trange[1]), fp(trange[1])
 
     # Predictor Step -- CN + explicit Euler
-    tfv = M*inivel - .5*dt*A*inivel + .5*dt*(fv_c+fv_n + bfv_n+bfv_c) \
-        + dt*nfc_c - (mbc_n-mbc_c)
+    tfv = M*inivel - .5*dt*A*inivel + .5*dt*(fv_c+fv_n + tbfv+bfv_c + tdfv+dfv_c) \
+        + dt*nfc_c - (tmbc-mbc_c)
 
     tvp_new, coeffmatlu = \
         lau.solve_sadpnt_smw(amat=M+.5*dt*A, jmat=J, jmatT=J.T,
                              rhsv=tfv,
-                             rhsp=fp_n+bfp_n,
+                             rhsp=fp_n+tbfp,
                              return_alu=True)
     tv_new = tvp_new[:NV, :]
     tp_new = 1./dt*scalep*tvp_new[NV:, :]
-    savevp(appndbcs(tv_new, bcs_n), tp_new, time=(trange[1], 'heunpred'))
+    savevp(appndbcs(tv_new, tbcs), tp_new, time=(trange[1], 'heunpred'))
 
     # Corrector Step
-    nfc_n = nonlvfunc(appndbcs(tv_new, bcs_n))
-    bcs_n = getbcs(trange[1], appndbcs(tv_new, bcs_n), tp_new, mode='heuncorr')
-
+    dfv_n, drm = dynamic_rhs(trange[1], vc=tv_new, memory=drm, mode='heunpred')
+    nfc_n = nonlvfunc(appndbcs(tv_new, tbcs))
+    bcs_n = getbcs(trange[1], appndbcs(tv_new, tbcs), tp_new, mode='heuncorr')
     bfv_n, bfp_n, mbc_n = applybcs(bcs_n)
-    rhs_n = M*inivel - .5*dt*A*inivel + .5*dt*(fv_c+fv_n + bfv_n+bfv_c +
-                                               nfc_c+nfc_n) - (mbc_n-mbc_c)
+    rhs_n = M*inivel - .5*dt*A*inivel - (mbc_n-mbc_c) +\
+        .5*dt*(fv_c+fv_n + bfv_n+bfv_c + dfv_n+tdfv + nfc_c+nfc_n)
 
     vp_new = coeffmatlu(np.vstack([rhs_n, fp_n+bfp_n]).flatten())
     v_new = vp_new[:NV].reshape((NV, 1))
@@ -80,6 +88,7 @@ def cnab(trange=None, inivel=None, inip=None, bcs_ini=[],
             bcs_c = bcs_n
             bfv_c, mbc_c = bfv_n, mbc_n
             fv_c = fv_n
+            dfv_c = dfv_n
 
             nfc_o = nfc_c
             nfc_c = nonlvfunc(appndbcs(v_old, bcs_c))
@@ -87,26 +96,26 @@ def cnab(trange=None, inivel=None, inip=None, bcs_ini=[],
             bcs_n = getbcs(ctime, appndbcs(v_old, bcs_c), p_old, mode='abtwo')
             bfv_n, bfp_n, mbc_n = applybcs(bcs_n)
             fv_n, fp_n = fv(ctime), fp(ctime)
+            dfv_n, drm = dynamic_rhs(trange[1], vc=v_old, memory=drm,
+                                     mode='abtwo')
 
-            rhs_n = M*v_old - .5*dt*A*v_old + .5*dt*(fv_c+fv_n + bfv_n+bfv_c) +\
-                1.5*dt*nfc_c-.5*dt*nfc_o - (mbc_n-mbc_c)
+            rhs_n = M*v_old - .5*dt*A*v_old + 1.5*dt*nfc_c-.5*dt*nfc_o \
+                - (mbc_n-mbc_c) \
+                + .5*dt*(fv_c+fv_n + bfv_n+bfv_c + dfv_n+dfv_c)
 
             vp_new = coeffmatlu(np.vstack([rhs_n, fp_n+bfp_n]).flatten())
+
             v_new = vp_new[:NV].reshape((NV, 1))
             p_new = 1./dt*scalep*vp_new[NV:].reshape((NP, 1))
 
             savevp(appndbcs(v_new, bcs_n), p_new, time=ctime)
-
-            nfc_o = nfc_c
-            bfv_c, mbc_c = bfv_n, mbc_n
-            fv_c = fv_n
 
     return v_new, p_new
 
 
 def get_cnab_linear_dynamic_fb(hb=None, ha=None, hc=None,
                                inihx=None, drift=None):
-    """ realizes the heun/AB2 discretization of a linear observer """
+    """ realizes the Heun/AB2 discretization of a linear observer """
     def cnab_dynamic_fb(t, cv=None, memory={}, mode='abtwo'):
         if mode == 'init':
             chx = inihx
