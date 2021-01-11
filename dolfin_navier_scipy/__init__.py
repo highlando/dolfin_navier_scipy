32df391f4408e99d76d6e97bedd239775d7fb795
Jan Heiland <jnhlnd@gmail.com>
2021-01-11 11:52:03 +0100

    working on the docs #12

--- dolfin_navier_scipy/__init__.py
+++ dolfin_navier_scipy/__init__.py
@@ -2,10 +2,11 @@ from . import dolfin_to_sparrays
 from . import data_output_utils
 from . import problem_setups
 from . import stokes_navier_utils
+from . import time_int_utils
 
 __all__ = ["dolfin_to_sparrays",
            "data_output_utils",
            "stokes_navier_utils",
            "problem_setups",
-           "time_step_schemes",
+           "time_int_utils",
            "residual_checks"]
