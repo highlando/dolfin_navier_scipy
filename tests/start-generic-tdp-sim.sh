041a16acda1168317d633f788f9349c3e341291e
Jan Heiland <jnhlnd@gmail.com>
2021-04-01 08:03:08 +0200

    upd setuppy

--- tests/start-generic-tdp-sim.sh
+++ tests/start-generic-tdp-sim.sh
@@ -2,9 +2,11 @@
 # PRVFRM=700
 # MSHPRFX='mesh/2D-double-rotcyl'
 
-MSHPRFX='/home/heiland/work/code/lqgbt-oseen/tests/mesh/2D-outlet-meshes/karman2D-outlets'
-MSHLVL=2
-RE=60
+MSHDIR='/home/heiland/work/code/lqgbt-oseen/tests/mesh/2D-outlet-meshes/'
+MSHNAM='karman2D-outlets'
+MSHPRFX=${MSHDIR}${MSHNAM}
+MSHLVL=1
+RE=40
 NTS=4200  # 4200 was OK with CNAB, 4000 not
 TE=4
 SCLTST=1.
