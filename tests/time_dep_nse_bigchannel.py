fixed/made consistent the weak formulations -- tests to check differences of symmetric and usymmetric velocity gradients

tests/time_dep_nse_bigchannel.py
@@ -1,11 +1,19 @@
 import os
-import numpy as np
+import logging
+# import numpy as np
 
 import dolfin_navier_scipy.stokes_navier_utils as snu
 import dolfin_navier_scipy.problem_setups as dnsps
 
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
 meshprfx = 'mesh/karman2D-outlets'
-meshlevel = 1
+meshlevel = 3
 meshfile = meshprfx + '_lvl{0}.xml.gz'.format(meshlevel)
 physregs = meshprfx + '_lvl{0}_facet_region.xml.gz'.format(meshlevel)
 geodata = meshprfx + '_geo_cntrlbc.json'
@@ -13,11 +21,13 @@ proutdir = 'results/'
 
 
 def testit(problem='drivencavity', N=None, nu=1e-2, Re=None,
-           t0=0.0, tE=1.0, Nts=1e2+1, ParaviewOutput=False, scheme='TH'):
+           t0=0.0, tE=1.0, Nts=1e2+1, ParaviewOutput=False, scheme='TH',
+           symmetricgrad=True):
 
     femp, stokesmatsc, rhsd = \
         dnsps.get_sysmats(problem='gen_bccont', Re=Re, bccontrol=False,
                           scheme=scheme, mergerhs=True,
+                          gradvsymmtrc=symmetricgrad,
                           meshparams=dict(strtomeshfile=meshfile,
                                           strtophysicalregions=physregs,
                                           strtobcsobs=geodata))
@@ -26,11 +36,10 @@ def testit(problem='drivencavity', N=None, nu=1e-2, Re=None,
         format(N, femp['Re'], Nts, tE, scheme)
 
     print('computing `c_mat`...')
-    import sadptprj_riclyap_adi.lin_alg_utils as lau
+    # import sadptprj_riclyap_adi.lin_alg_utils as lau
     import distributed_control_fenics.cont_obs_utils as cou
-    mc_mat, y_masmat = cou.get_mout_opa(odcoo=femp['odcoo'],
-                                        V=femp['V'], mfgrid=(3, 1))
-    c_mat = lau.apply_massinv(y_masmat, mc_mat, output='sparse')
+    c_mat = cou.get_mout_opa(odcoo=femp['odcoo'], V=femp['V'], mfgrid=(3, 1))
+    # c_mat = lau.apply_massinv(y_masmat, mc_mat, output='sparse')
     # restrict the operator to the inner nodes
     c_mat = c_mat[:, femp['invinds']][:, :]
 
@@ -46,6 +55,7 @@ def testit(problem='drivencavity', N=None, nu=1e-2, Re=None,
         raise Warning('need "' + ddir + '" subdir for storing the data')
     os.chdir('..')
 
+    svgstr = '' if symmetricgrad else '_nsmg'
     soldict = stokesmatsc  # containing A, J, JT
     soldict.update(femp)  # adding V, Q, invinds, diribcs
     soldict.update(tips)  # adding time integration params
@@ -56,25 +66,29 @@ def testit(problem='drivencavity', N=None, nu=1e-2, Re=None,
                    treat_nonl_explicit=True,
                    dbcinds=femp['dbcinds'], dbcvals=femp['dbcvals'],
                    data_prfx=ddir+data_prfx,
-                   paraviewoutput=ParaviewOutput,
-                   vfileprfx=proutdir+'vel_',
-                   pfileprfx=proutdir+'p_')
+                   paraviewoutput=ParaviewOutput, prvoutpnts=100,
+                   vfileprfx=proutdir+f'vel{svgstr}msh{meshlevel}_',
+                   pfileprfx=proutdir+f'p{svgstr}msh{meshlevel}_')
 
 #
 # compute the uncontrolled steady state Navier-Stokes solution
 #
     # v_ss_nse, list_norm_nwtnupd = snu.solve_steadystate_nse(**soldict)
     snu.solve_nse(**soldict)
-    print('for plots check \nparaview ' + proutdir + 'vel___timestep.pvd')
-    print('or \nparaview ' + proutdir + 'p___timestep.pvd')
+    print('for plots check \nparaview ' + proutdir +
+          f'vel{svgstr}msh{meshlevel}___timestep.pvd')
+    print('or \nparaview ' + proutdir +
+          f'p{svgstr}msh{meshlevel}___timestep.pvd')
 
 
 if __name__ == '__main__':
     scheme = 'TH'
-    Re = 100
+    Re = 30
     t0, tE, Nts = 0., 5., 4*2048
-    scaletest = .1
+    scaletest = 2.
+    symgrad = False
+    symgrad = True
 
     testit(problem='gen_bccont', Re=Re,
-           t0=scaletest*t0, tE=scaletest*tE, Nts=np.int(scaletest*Nts),
-           scheme=scheme, ParaviewOutput=True)
+           t0=scaletest*t0, tE=scaletest*tE, Nts=int(scaletest*Nts),
+           scheme=scheme, ParaviewOutput=True, symmetricgrad=symgrad)